aedd5ef57c6d81c9d0d7de1908762dc4bef567cc
tanujkathuria <tanujkathuria.kathuria@gmail.com>
2023-10-13 21:34:17 +0530

    fix: check in for the commits

--- src/main/java/com/developersworld/youtubetrending/configuration/BatchConfiguration.java
+++ src/main/java/com/developersworld/youtubetrending/configuration/BatchConfiguration.java
@@ -4,6 +4,7 @@ import com.developersworld.youtubetrending.DataInput;
 import com.developersworld.youtubetrending.listener.JobCompletionNotificationListener;
 import com.developersworld.youtubetrending.model.Data;
 import com.developersworld.youtubetrending.processor.DataProcessing;
+import java.net.MalformedURLException;
 import javax.sql.DataSource;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -20,6 +21,8 @@ import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -43,10 +46,16 @@ public class BatchConfiguration {
   };
 
   @Bean
-  public FlatFileItemReader<DataInput> reader() {
+  public FlatFileItemReader<DataInput> reader() throws MalformedURLException {
+    // UrlResource resource = new UrlResource(
+    //   "https://youtube-analyzer-bucket.s3.ap-south-1.amazonaws.com/youtube_trending_data.csv"
+    // );
+
     return new FlatFileItemReaderBuilder<DataInput>()
       .name("personItemReader")
       .resource(new ClassPathResource("youtube_trending_data.csv"))
+      // this is required for the AWS hosting
+      // .resource(resource)
       .delimited()
       .names(FIELD_NAMES)
       .fieldSetMapper(
@@ -98,7 +107,8 @@ public class BatchConfiguration {
     JobRepository jobRepository,
     PlatformTransactionManager transactionManager,
     JdbcBatchItemWriter<Data> writer
-  ) {
+  )
+    throws MalformedURLException {
     return new StepBuilder("step1", jobRepository)
       .<DataInput, Data>chunk(10, transactionManager)
       .reader(reader())
